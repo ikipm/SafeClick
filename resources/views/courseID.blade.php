e2e8fc48630b53ef914f7b0fd81a8381afd92748
ikipm <ikipm0@gmail.com>
2023-08-31 19:29:33 +0200

    Add: Content view

--- resources/views/courseID.blade.php
+++ resources/views/courseID.blade.php
@@ -11,18 +11,60 @@
     <header>
         <x-navbar />
     </header>
-
     <main>
-        @php
-        use Illuminate\Support\Facades\Session;
-        $locale = Session::get('locale', 'cat');
-        @endphp
-        <section class="cards-section">
-            <div class="container">
-                <h2>{{ $course->translations->where('locale', $locale)->first()->title }}</h2>
+        <section class="course-section">
+            <div class="course-card">
+                <div class="card-header">
+                    <h1>Title</h1>
+                </div>
+                <div class="course-content">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet non augue volutpat commodo. Duis ipsum ligula, semper a sapien non, aliquam imperdiet lectus. Pellentesque dignissim eget urna vel sagittis. Nam eu luctus dui. Aenean lacinia rutrum turpis, nec ultrices erat rutrum et. Ut porta aliquam enim. Quisque sollicitudin facilisis scelerisque.
+
+Sed tempus ante eget ante pharetra, quis aliquam ligula luctus. Aliquam dui nisi, rhoncus ac sodales nec, auctor ut lacus. Maecenas laoreet mollis velit. Aliquam maximus mauris eget tortor malesuada, ut pretium diam tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec eget ultrices massa. Sed elit dui, accumsan maximus ultrices vitae, auctor sed est.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus sodales dignissim urna, id molestie nunc tristique a. Quisque facilisis, ipsum eu ultrices fermentum, enim odio feugiat arcu, sed ornare velit felis quis nisi. Suspendisse gravida at leo vel pretium. Sed ut rhoncus urna. Morbi arcu justo, aliquet blandit elementum non, luctus vitae nunc. Quisque ultricies justo lorem, nec pretium arcu pharetra in. Suspendisse potenti. Morbi congue sed risus ac aliquet. Fusce tempor ante sit amet sapien consectetur, quis ultrices ipsum eleifend. Praesent a dolor odio.
+
+Cras dapibus, dui et blandit molestie, tortor magna vehicula purus, in dictum sapien lacus at metus. Ut non facilisis magna, eget auctor sem. Phasellus lobortis auctor felis vel consectetur. Nam porta leo ligula, vel posuere orci lobortis sed. Vivamus ultrices fermentum bibendum. Sed tempus, diam in pulvinar pharetra, tellus erat elementum nisi, non auctor tortor nibh a lorem. Aliquam erat urna, condimentum non lobortis non, scelerisque vitae diam. Duis posuere purus eu libero laoreet, quis pharetra nulla scelerisque. Sed venenatis nisi quam, aliquet luctus augue bibendum sollicitudin. In tristique congue nunc, et varius nisi interdum ac. Aenean suscipit convallis tortor, a sodales tortor semper non. Maecenas euismod risus id lectus auctor dictum. Nam congue tortor at massa pulvinar, tincidunt efficitur sem finibus.
+
+Donec lorem odio, accumsan ut eros ac, sagittis sodales nisi. Nulla at sapien eu felis vulputate suscipit non at erat. Morbi eu nisl a lectus interdum venenatis. Nunc vel mauris nec nibh tempus aliquet non et mi. Quisque eu bibendum erat, a facilisis ante. Aliquam aliquet scelerisque risus, sed interdum nibh convallis egestas. Quisque feugiat, turpis rutrum accumsan suscipit, libero mi convallis erat, a molestie tellus lectus in dui. Vivamus nec eros in leo molestie tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas sed laoreet urna
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet non augue volutpat commodo. Duis ipsum ligula, semper a sapien non, aliquam imperdiet lectus. Pellentesque dignissim eget urna vel sagittis. Nam eu luctus dui. Aenean lacinia rutrum turpis, nec ultrices erat rutrum et. Ut porta aliquam enim. Quisque sollicitudin facilisis scelerisque.
+
+Sed tempus ante eget ante pharetra, quis aliquam ligula luctus. Aliquam dui nisi, rhoncus ac sodales nec, auctor ut lacus. Maecenas laoreet mollis velit. Aliquam maximus mauris eget tortor malesuada, ut pretium diam tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec eget ultrices massa. Sed elit dui, accumsan maximus ultrices vitae, auctor sed est.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus sodales dignissim urna, id molestie nunc tristique a. Quisque facilisis, ipsum eu ultrices fermentum, enim odio feugiat arcu, sed ornare velit felis quis nisi. Suspendisse gravida at leo vel pretium. Sed ut rhoncus urna. Morbi arcu justo, aliquet blandit elementum non, luctus vitae nunc. Quisque ultricies justo lorem, nec pretium arcu pharetra in. Suspendisse potenti. Morbi congue sed risus ac aliquet. Fusce tempor ante sit amet sapien consectetur, quis ultrices ipsum eleifend. Praesent a dolor odio.
+
+Cras dapibus, dui et blandit molestie, tortor magna vehicula purus, in dictum sapien lacus at metus. Ut non facilisis magna, eget auctor sem. Phasellus lobortis auctor felis vel consectetur. Nam porta leo ligula, vel posuere orci lobortis sed. Vivamus ultrices fermentum bibendum. Sed tempus, diam in pulvinar pharetra, tellus erat elementum nisi, non auctor tortor nibh a lorem. Aliquam erat urna, condimentum non lobortis non, scelerisque vitae diam. Duis posuere purus eu libero laoreet, quis pharetra nulla scelerisque. Sed venenatis nisi quam, aliquet luctus augue bibendum sollicitudin. In tristique congue nunc, et varius nisi interdum ac. Aenean suscipit convallis tortor, a sodales tortor semper non. Maecenas euismod risus id lectus auctor dictum. Nam congue tortor at massa pulvinar, tincidunt efficitur sem finibus.
+
+Donec lorem odio, accumsan ut eros ac, sagittis sodales nisi. Nulla at sapien eu felis vulputate suscipit non at erat. Morbi eu nisl a lectus interdum venenatis. Nunc vel mauris nec nibh tempus aliquet non et mi. Quisque eu bibendum erat, a facilisis ante. Aliquam aliquet scelerisque risus, sed interdum nibh convallis egestas. Quisque feugiat, turpis rutrum accumsan suscipit, libero mi convallis erat, a molestie tellus lectus in dui. Vivamus nec eros in leo molestie tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas sed laoreet urna
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet non augue volutpat commodo. Duis ipsum ligula, semper a sapien non, aliquam imperdiet lectus. Pellentesque dignissim eget urna vel sagittis. Nam eu luctus dui. Aenean lacinia rutrum turpis, nec ultrices erat rutrum et. Ut porta aliquam enim. Quisque sollicitudin facilisis scelerisque.
+
+Sed tempus ante eget ante pharetra, quis aliquam ligula luctus. Aliquam dui nisi, rhoncus ac sodales nec, auctor ut lacus. Maecenas laoreet mollis velit. Aliquam maximus mauris eget tortor malesuada, ut pretium diam tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec eget ultrices massa. Sed elit dui, accumsan maximus ultrices vitae, auctor sed est.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus sodales dignissim urna, id molestie nunc tristique a. Quisque facilisis, ipsum eu ultrices fermentum, enim odio feugiat arcu, sed ornare velit felis quis nisi. Suspendisse gravida at leo vel pretium. Sed ut rhoncus urna. Morbi arcu justo, aliquet blandit elementum non, luctus vitae nunc. Quisque ultricies justo lorem, nec pretium arcu pharetra in. Suspendisse potenti. Morbi congue sed risus ac aliquet. Fusce tempor ante sit amet sapien consectetur, quis ultrices ipsum eleifend. Praesent a dolor odio.
+
+Cras dapibus, dui et blandit molestie, tortor magna vehicula purus, in dictum sapien lacus at metus. Ut non facilisis magna, eget auctor sem. Phasellus lobortis auctor felis vel consectetur. Nam porta leo ligula, vel posuere orci lobortis sed. Vivamus ultrices fermentum bibendum. Sed tempus, diam in pulvinar pharetra, tellus erat elementum nisi, non auctor tortor nibh a lorem. Aliquam erat urna, condimentum non lobortis non, scelerisque vitae diam. Duis posuere purus eu libero laoreet, quis pharetra nulla scelerisque. Sed venenatis nisi quam, aliquet luctus augue bibendum sollicitudin. In tristique congue nunc, et varius nisi interdum ac. Aenean suscipit convallis tortor, a sodales tortor semper non. Maecenas euismod risus id lectus auctor dictum. Nam congue tortor at massa pulvinar, tincidunt efficitur sem finibus.
+
+Donec lorem odio, accumsan ut eros ac, sagittis sodales nisi. Nulla at sapien eu felis vulputate suscipit non at erat. Morbi eu nisl a lectus interdum venenatis. Nunc vel mauris nec nibh tempus aliquet non et mi. Quisque eu bibendum erat, a facilisis ante. Aliquam aliquet scelerisque risus, sed interdum nibh convallis egestas. Quisque feugiat, turpis rutrum accumsan suscipit, libero mi convallis erat, a molestie tellus lectus in dui. Vivamus nec eros in leo molestie tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas sed laoreet urna
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet non augue volutpat commodo. Duis ipsum ligula, semper a sapien non, aliquam imperdiet lectus. Pellentesque dignissim eget urna vel sagittis. Nam eu luctus dui. Aenean lacinia rutrum turpis, nec ultrices erat rutrum et. Ut porta aliquam enim. Quisque sollicitudin facilisis scelerisque.
+
+Sed tempus ante eget ante pharetra, quis aliquam ligula luctus. Aliquam dui nisi, rhoncus ac sodales nec, auctor ut lacus. Maecenas laoreet mollis velit. Aliquam maximus mauris eget tortor malesuada, ut pretium diam tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec eget ultrices massa. Sed elit dui, accumsan maximus ultrices vitae, auctor sed est.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus sodales dignissim urna, id molestie nunc tristique a. Quisque facilisis, ipsum eu ultrices fermentum, enim odio feugiat arcu, sed ornare velit felis quis nisi. Suspendisse gravida at leo vel pretium. Sed ut rhoncus urna. Morbi arcu justo, aliquet blandit elementum non, luctus vitae nunc. Quisque ultricies justo lorem, nec pretium arcu pharetra in. Suspendisse potenti. Morbi congue sed risus ac aliquet. Fusce tempor ante sit amet sapien consectetur, quis ultrices ipsum eleifend. Praesent a dolor odio.
+
+Cras dapibus, dui et blandit molestie, tortor magna vehicula purus, in dictum sapien lacus at metus. Ut non facilisis magna, eget auctor sem. Phasellus lobortis auctor felis vel consectetur. Nam porta leo ligula, vel posuere orci lobortis sed. Vivamus ultrices fermentum bibendum. Sed tempus, diam in pulvinar pharetra, tellus erat elementum nisi, non auctor tortor nibh a lorem. Aliquam erat urna, condimentum non lobortis non, scelerisque vitae diam. Duis posuere purus eu libero laoreet, quis pharetra nulla scelerisque. Sed venenatis nisi quam, aliquet luctus augue bibendum sollicitudin. In tristique congue nunc, et varius nisi interdum ac. Aenean suscipit convallis tortor, a sodales tortor semper non. Maecenas euismod risus id lectus auctor dictum. Nam congue tortor at massa pulvinar, tincidunt efficitur sem finibus.
+
+Donec lorem odio, accumsan ut eros ac, sagittis sodales nisi. Nulla at sapien eu felis vulputate suscipit non at erat. Morbi eu nisl a lectus interdum venenatis. Nunc vel mauris nec nibh tempus aliquet non et mi. Quisque eu bibendum erat, a facilisis ante. Aliquam aliquet scelerisque risus, sed interdum nibh convallis egestas. Quisque feugiat, turpis rutrum accumsan suscipit, libero mi convallis erat, a molestie tellus lectus in dui. Vivamus nec eros in leo molestie tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas sed laoreet urna
+                    </p>
+                </div>
+                <div class="course-buttons">
+                    <button class="button-prev">&#8592; Previous</button>
+                    <button class="button-next">Next &#8594;</button>
+                </div>
             </div>
         </section>
     </main>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/courseView.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/platform.css') }}">
 </body>
 
