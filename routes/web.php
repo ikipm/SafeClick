f16ab91549317eca63b1b15f870802db7a4bf22e
ikipm <ikipm0@gmail.com>
2023-09-01 13:56:39 +0200

    Update: Structured routes

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::post('/register', [UserController::class, 'register'])->name('register');
 Route::post('/admin/register', [UserController::class, 'adminRegister'])->middleware('auth')->middleware('admin')->name('admin.register');
 Route::get('/admin/search', [UserController::class, 'adminSearch'])->middleware('auth')->middleware('admin')->name('admin.search');
 Route::post('/admin/courses/create', [CourseController::class, 'store'])->middleware('auth')->middleware('admin')->name('admin.createCourse');
-Route::post('admin/create-course', [CourseController::class, 'store'])->middleware('auth')->middleware('admin')->name('admin.createCourse');
-Route::get('admin/search-course', [CourseController::class, 'search'])->middleware('auth')->middleware('admin')->name('admin.searchCourse');
+Route::post('admin/course/create', [CourseController::class, 'store'])->middleware('auth')->middleware('admin')->name('admin.createCourse');
+Route::get('admin/course/search', [CourseController::class, 'search'])->middleware('auth')->middleware('admin')->name('admin.searchCourse');
 Route::post('admin/courses/edit/{id}', [CourseController::class, 'update'])->middleware('auth')->middleware('admin')->name('admin.editCourse');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
@@ -38,5 +38,5 @@ Route::group(['middleware' => 'setLocale'], function () {
     Route::view('/admin', 'admin')->middleware('auth')->middleware('admin')->name('admin');
     Route::view('/admin/users', 'admin.users')->middleware('auth')->middleware('admin')->name('admin.users');
     Route::view('/admin/courses', 'admin.courses')->middleware('auth')->middleware('admin')->name('admin.courses');
-    Route::get('/admin/courses/edit/{courseId}', [CourseController::class, 'courseEditInfo'])->middleware('auth')->middleware('admin')->name('admin.coursesEdit');
+    Route::get('/admin/courses/edit/{courseId}', [CourseController::class, 'courseEditInfo'])->middleware('auth')->middleware('admin')->name('admin.coursesEdit');  // This view is in Course controller
 });
