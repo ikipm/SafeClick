c0f01fd2cb3cc569c0f4a429d1c209df3513acb2
ikipm <ikipm0@gmail.com>
2023-09-02 13:29:12 +0200

    Add: Course button to course view

--- resources/views/courseID.blade.php
+++ resources/views/courseID.blade.php
@@ -34,12 +34,18 @@
                     <a href="/courses/{{$content->course_id}}/{{$content->content_id - 1}}" class="button-prev">&#8592; @lang('courses.previous')</a>
                     @endif
                     @if($content->content_id !== $content->count() / 3)
-                        <a href="/courses/{{$content->course_id}}/{{$content->content_id + 1}}" class="button-next">@lang('courses.next') &#8594;</a>
+                    <a href="/courses/{{$content->course_id}}/{{$content->content_id + 1}}" class="button-next">@lang('courses.next') &#8594;</a>
                     @endif
                 </div>
             </div>
         </section>
     </main>
+    <a class="fixed-button" href="/courses">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+            <path d="M12 2L0 12h3v10h18V12h3z" />
+            <path fill="none" d="M0 0h24v24H0z" />
+        </svg>
+    </a>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/courseView.css') }}">
 </body>
 
